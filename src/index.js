29a13929a68166ba64e4756437276070dd208839
Jordan <jordanleelong5@gmail.com>
2020-06-04 22:54:07 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,36 +1,18 @@
 let addToy = false;
 
-
-
-function getToys() {
-  fetch("http://localhost:3000/toys")
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function(toys) {
-    toys.forEach(function(toy) {
-      createToyCard(toy)
-    })
-   
-  })
-  .catch(function(error) {
-    console.log(error)
-  })
-}
-
 function createToyCard(toy) {
 
-    let newDiv = document.createElement('div')
-    newDiv.classList.add('card')
+  console.log(toy.image)
+  let getDiv = document.getElementById('toy-collection');
+    let newDiv = document.createElement('div');
+    newDiv.classList.add('card');
 
-      let getDiv = document.getElementById('toy-collection')
-      // getDiv.classList.add('card');
 
-      let toyName = document.createElement('h2')
-      toyName.textContent = toy.name
+      let toyName = document.createElement('h2');
+      toyName.innerText = toy.name;
 
-      let imgTag = document.createElement("img")
-      imgTag.setAttribute("src", toy.image);
+      let imgTag = document.createElement("img");
+      imgTag.setAttribute("src", toy.image );
       imgTag.setAttribute("class", "toy-avatar");
       // imgTag.textContent = toy.image
 
@@ -54,8 +36,27 @@ function createToyCard(toy) {
 
 }
 
-function addNewToy(newName, newImage, newLike) {
-  // console.log(newToy)
+
+function getToys() {
+  fetch("http://localhost:3000/toys")
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(toys) {
+    toys.forEach(function(toy) {
+      createToyCard(toy)
+    })
+   
+  })
+  .catch(function(error) {
+    console.log(error)
+  })
+}
+
+
+function addNew(newToy) {
+
+  console.log(newToy)
 
   fetch("http://localhost:3000/toys", {
     method: "POST",
@@ -63,39 +64,27 @@ function addNewToy(newName, newImage, newLike) {
       "Content-Type": "application/json",
       "Accept": "application/json"
     },
-    body: JSON.stringify( {
-      "name": newName,
-      "image": newImage,
-      likes: newLike
+    body: JSON.stringify({
+      name: newToy.name,
+      image: newToy.image,
+      likes: 0
     })
-
     })
 
     .then(function(response) {
       return response.json();
     })
     .then(function(object) {
+      // console.log(object)
       createToyCard(object)
-    })
-    .catch(function(error) {
-      console.log(error)
     })
   
 }
 
 function addLikes(e) {
   event.preventDefault();
-    // dig through e, last sibling? inner text 
-  // pass in e target id in the fetch
-  // set likes to the new number
-  // set the first task on line 58 == `${newNumber} likes
-
-  // console.log(e.target.previousSibling.innerText)
 
   let newNum = parseInt(e.target.previousElementSibling.innerText) + 1
-
-  
- 
 
   fetch(`http://localhost:3000/toys/${e.target.id}`, {
     method: "PATCH",
@@ -114,7 +103,7 @@ function addLikes(e) {
     })
     .then(function() {
       // console.log(object)
-      e.target.previousElementSibling.innerText = `${newNum}`
+      e.target.previousElementSibling.innerText = `${newNum} likes`
     })
     .catch(function(error) {
       console.log(error)
@@ -124,13 +113,21 @@ function addLikes(e) {
 
 
 
-
 document.addEventListener("DOMContentLoaded", () => {
+  getToys();
   const addBtn = document.querySelector("#new-toy-btn");
   const toyFormContainer = document.querySelector(".container");
-  getToys();
+
+  let submit = document.querySelector(".submit").addEventListener("submit", function(event) {
+  
+    event.preventDefault();
+  let newToy = Object.assign({}, {name: event.target[0].value}, {image: event.target[1].value})
+  addNew(newToy);
+
+  })
+
   addBtn.addEventListener("click", () => {
-    addNewToy();
+    // addNew();
     // hide & seek with the form
     addToy = !addToy;
     if (addToy) {
