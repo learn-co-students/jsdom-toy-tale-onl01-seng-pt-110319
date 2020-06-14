70d4d54b2d3a85d9f7b48487a3578a6a41ca6a58
Alouiscious Media <info@reneesoul.com>
2020-06-14 02:25:52 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,80 @@
 let addToy = false;
 
+// insertToys(allToys)
 document.addEventListener("DOMContentLoaded", () => {
-  const addBtn = document.querySelector("#new-toy-btn");
-  const toyFormContainer = document.querySelector(".container");
-  addBtn.addEventListener("click", () => {
+  getAllToys()
+
+  // add all toys to index.html from the api database
+  function getAllToys() {
+    return fetch('http://localhost:3000/toys')
+    .then(resp => resp.json())
+    .then(toy => {toy.forEach(toy => {
+      let html = `
+        <div class="card">
+          <h2 class='toyName'>${toy.name}</h2>
+          <img src="${toy.image}" width="200"/>
+          <p>${toy.likes} likes!</p>
+          <button>Like</button>
+        </div>
+      `
+      if (toy.image) {
+        document.querySelector("#toy-collection").insertAdjacentHTML("beforeend", html)
+      }
+    })})
+         
+  };
+        
+  // add new toys to database
+
+  let configObj = {
+    method: "POST",
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    },
+    body: JSON.stringify(newHtml)
+  };
+  function newToy() {
+
+    return fetch('http://localhost:3000/toys', configObj)
+    .then(resp => resp.json())
+    
+    .then(json => { (json)
+      
+        let newHtml = `
+        <h3>Recently Added!</h3>
+        <h2 class='toyName'>${newToy.name}</h2>
+        <img src="${json.image}"/>
+        <p>${json.likes} likes!</p>
+        <button>Like</button>
+        `
+        // debugger
+        document.querySelector("#new-toy-btn").insertAdjacentHTML("afterend", newHtml)
+    })
+      .catch((error) => {   // how to manage when fetch goes wrong
+        alert("Nope! That wasn't it!"),
+        document.body.innerHTML = error.message
+      });
+    }
+  
+
+
+  // Event Listeners
+  const toyForm = document.querySelector(".container");
+  toyForm.addEventListener('submit', s => { 
+    s.preventDefault(),
+    newToy();
+  });
+  document.addEventListener("click", (e) => {
     // hide & seek with the form
-    addToy = !addToy;
-    if (addToy) {
-      toyFormContainer.style.display = "block";
-    } else {
-      toyFormContainer.style.display = "none";
+    if (e.target.matches("#new-toy-btn")) {
+      addToy = !addToy
+      if (addToy) {
+        toyForm.style.display = "block";
+      } else {
+        toyForm.style.display = "none";
+      }
     }
   });
+
 });
